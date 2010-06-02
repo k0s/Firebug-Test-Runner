4474ce03d485dea74d545e27cc7bac692929bc01
Jeff Hammel <jhammel@mozilla.com>
2010-06-02 14:39:35 -0700

    comments

--- fb-run.py
+++ fb-run.py
@@ -2,8 +2,15 @@ from ConfigParser import ConfigParser
 from time import sleep
 import os, sys, signal, subprocess, shlex, getopt
 
+### you use several different ways of running processes:
+### - os.popen
+### - os.system
+### - subprocess.Popen
+### Standardize on one, namely on subprocess.Popen and subprocess.call
+
 def ping_firefox():
     "See if Firefox is still running"
+    # mozrunner already has this function....use it!
     for line in os.popen("ps xa"):
         fields = line.split()
         pid = fields[0]
@@ -14,6 +21,8 @@ def ping_firefox():
 
 def kill_firefox():
     "Kill Firefox if it is running"
+    # again, mozrunner already has this
+    # you should probably make your code a real package and depend on this
     pid = ping_firefox()
     if pid > 0:
         os.kill(int(pid), signal.SIGHUP)
@@ -23,11 +32,14 @@ def cleanup(exit_code):
     os.system("rm ./firebug.xpi")
     os.system("rm ./fbtest.xpi")
     sys.exit(exit_code)
+    # cleanup probably shouldn't exit the function, too magical
 
 # Initialization
 config = ConfigParser()
 try:
     config.read("./fb-test-runner.config")
+    # depending on having this file in the same directory is silly
+    # probably the easiest thing to do is to pass it in from the command line
 except ConfigParser.NoSectionError:
     print "[Error] Could not find 'fb-test-runner.config'"
     sys.exit(1)
@@ -41,13 +53,18 @@ try:
 except getopt.GetoptError, err:
     print str(err) 
     sys.exit(2)
+### NEVER use getopt in python!  You will always ...
+### - write more code
+### - write code that is harder to understand and maintain
+### - have horrible if/else trees like you do below!
+### Use optparse.OptionParser much nicer!
 
 for o, a in opts:
     if o == "-s":
         serverpath = a
     elif o == "-v":
         firebug_version = a
-    elif 0 == "-p":
+    elif 0 == "-p": # pretty sure that 0 is a typ0
         profile = a 
     else:
         assert False, "Unhandled command line option"
@@ -55,6 +72,15 @@ for o, a in opts:
 if config.get("run", "auto_profile").lower() == "on":
     # Attempt to find the default profile automatically
     p = subprocess.Popen("locate -r \\\\.mozilla/firefox.*\\\\.default$", shell=True, stdout=subprocess.PIPE)
+    ### this is a really silly way of doing this
+    ### also, isn't the logic a bit weird?  You look up the profile in the config, which may be overridden by command line options, which may be overridden by a config setting to magically look it up, which if it fails falls back?!?
+    # at least document this
+    profile_dir = os.path.join(os.environ['HOME'], '.mozilla', 'firefox')
+    profiles = os.listdir(profile_dir)
+    _profile = [ os.path.join(profile_dir, p) for p in profiles
+                 if p.endswith('.default') ]
+    if _profile:
+        profile = _profile[0]
     try:
         profile = p.communicate()[0]
         profile = str(profile[:-1])
@@ -63,16 +89,17 @@ if config.get("run", "auto_profile").lower() == "on":
 
 # Ensure the profile actually exists
 if not os.path.exists(profile + "/prefs.js"):
+    ### ALWAYS use os.path.join!
     print "[Error] Profile doesn't exist: " + profile
     sys.exit(1)
 
 # Concatenate serverpath based on Firebug version
-if serverpath[-1:] != "/":
+if serverpath[-1:] != "/": 
     serverpath = serverpath + "/firebug" + firebug_version
 else:
     serverpath = serverpath + "firebug" + firebug_version
 # If the extensions were somehow left over from last time, delete them to ensure we don't accidentally run
-if os.path.exists("./firebug.xpi"):
+if os.path.exists("./firebug.xpi"): # again, os.path.join, but, um, why even bother since they are relative to cwd?
     os.system("rm ./firebug.xpi")
 if os.path.exists("./fbtest.xpi"):
     os.system("rm ./fbtest.xpi")
@@ -88,9 +115,14 @@ os.system("mv " + profile + "/firebug/fbtest/logs/* " + profile + "/firebug/fbte
 
 # If firefox is running, kill it (needed for mozrunner)
 kill_firefox()
-sleep(2)
+sleep(2) # document all magic numbers
 # Install the two extensions using mozrunner
 subprocess.Popen(shlex.split("mozrunner -n -p " + profile + " --addons=\"./firebug.xpi\",\"./fbtest.xpi\""))
+# um, what?
+# firstly, you know the arugments so you don't have to split using shlex
+# secondly, if you did want to use a string to call here you would pass shell=True
+# thirdly, this isn't guaranteed to complete since you don't call communicate; use subprocess.call instead
+
 sleep(5)
 # Run Firefox with -runFBTests command line option (has to be run from shell otherwise log file won't be created)
 subprocess.Popen("firefox -runFBTests " + serverpath + "/tests/content/testlists/firebug" + firebug_version + ".html", shell=True)
